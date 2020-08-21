dbd0e052d77c0ea080a74325b588203cfad08edb
woffkass <woffkass@gmail.com>
2020-08-21 20:06:30 +0700

    шаблон сайта потфолио

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true,
+    },
+});
